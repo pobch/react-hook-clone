0d9416986c5847f6310d6c115c5f5b4e3c1c9e97
Pob <590650@gmail.com>
2019-04-17 00:40:35 +0700

    added comment

--- script1.js
+++ script1.js
@@ -26,11 +26,22 @@ const App = () => {
   }
 }
 
-const exe = App()
+// Flaws:
+// 1. `state` returned from `useState` is a function, we want it to be variable
+// 2. `App` functional component can be called only once, otherwise we will lose the state
+
+// call `App` component
+let exe = App()
 
 exe.render()
 exe.click()
 exe.render()
 exe.click()
 exe.click()
+exe.render()
+
+// if we call the component 2nd time
+console.log('======================')
+exe = App()
+// we will lose the state
 exe.render()
